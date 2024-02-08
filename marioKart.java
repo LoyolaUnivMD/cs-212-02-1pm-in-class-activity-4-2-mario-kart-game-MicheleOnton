3c971dbe11523fdf1b56648a019435b0dd4317db
Michele Onton <maontoncueva@loyola.edu>
2024-02-08 14:10:43 -0500

    marioKart is done

--- marioKart.java
+++ marioKart.java
@@ -1,12 +1,34 @@
-// Read the README.md
-// Read it again
-// Your code here
-// Delete these 4 lines of comments
 
-class HelloWorld {
+
+public class marioKart {
+    String playerName;
+    String character;
+    int speed;
+
+    public marioKart(String playerName, String character ,int speed) {
+        this.playerName = playerName;
+        this.character = character;
+        this.speed = speed;
+    }
+
+    public void boost(int boostAmount) {
+
+        this.speed += boostAmount;
+    }
+
+    public void displayinfo() {
+        System.out.println("Player Name: " + playerName);
+        System.out.println("Character: " + character);
+        System.out.println("Speed: " + speed);
+    }
+
     public static void main(String[] args) {
 
         System.out.println("Mario Kart!");
+            marioKart player1 = new marioKart("Julio", "Mario" ,10);
+        player1.displayinfo();
+        player1.boost(5);
+        System.out.println(player1.playerName + " speed after boost: " + player1.speed);
         
     }
 }
